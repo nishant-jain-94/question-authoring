bcc2a81b2e9337dc96d3a5f2f69fa6abee2e2a28
Nishant Jain <nishantkumarajain@gmail.com>
2017-10-17 15:05:56 +0530

    Removed the functionality to add players to players.json

--- install-dependencies.js
+++ install-dependencies.js
@@ -4,7 +4,7 @@ const request = require('request');
 const { exec } = require('child_process');
 
 request('https://morning-peak-73646.herokuapp.com/players', (error, response, players) => {
-  fs.writeFileSync('players.json', JSON.stringify(players));
+  // fs.writeFileSync('players.json', JSON.stringify(players));
   const bowerDependencies = JSON.parse(players).join(' ');
   if(bowerDependencies) {
     exec(`./node_modules/.bin/bower install ${bowerDependencies} --save`, (err, stdout, stderr) => {
